1bc668eb7357ed9c4ba4bc95b5dffabd2c003555
Balbaka <oce.cuvelier@gmail.com>
2019-10-28 14:22:14 +0100

    Attribut ++ constructeur de la classe equipe

--- src/be/cuvelier/Equipe.java
+++ src/be/cuvelier/Equipe.java
@@ -1,5 +1,17 @@
 package be.cuvelier;
 
 public class Equipe {
+	
+	private Joueur eq[];
+	private int nb_joueur;
+	
+	public Equipe(int nb) 
+	{
+		if(nb<= 2) {
+		eq = new Joueur[nb];
+		}else {
+			System.out.print("Impossible de créer de plus grande equipe");
+		}
+	}
 
 }
