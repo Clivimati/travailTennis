65c29f0d7810abf619ff39d6e08eef4b0357880d
Balbaka <oce.cuvelier@gmail.com>
2019-11-21 18:32:06 +0100

    ok

--- src/be/cuvelier/Match.java
+++ src/be/cuvelier/Match.java
@@ -5,12 +5,24 @@ import java.util.Random;
 
 public class Match {
 	
-	
-	
+	private Ordonnancement ord;
 	private int durer;
-	private int tour;
+	private int tour= 0;
 	private Date date;
 	private int score;
+	
+	public Match() {}
+	
+	@SuppressWarnings("deprecation")
+	public Match(Equipe[]  t ) {
+		ord.GetType();
+		int temps = new Random().nextInt((3 - 1) + 1) + 1;
+		durer = temps;
+		int month  = new Random().nextInt((7 - 5) + 1) + 5;
+		int jour = new Random().nextInt((30 - 1) + 1) + 1;
+		int year= 2020;
+		date = new Date(year, month, jour);
+	}
 	
 	public int GetScore() {
 		return score;
@@ -28,10 +40,17 @@ public class Match {
 		return date;
 	}
 	
+	public Equipe[] JouerMatch(Equipe e1 , Equipe e2) {
+		
+		Equipe [] team = new Equipe [2];
+		team[1]= e1;
+		team[2]=e2;
+		return team;	
+	}
+	
 	public void VerifierScore(Equipe[] team) 
 	{
 		int set = 1;
-		Random r = new Random();
-		r.nextInt(7 - 4);	
+		
 	}
 }
