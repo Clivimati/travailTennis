431e42a38cc340d813fea23a9427f5a686116be9
Balbaka <oce.cuvelier@gmail.com>
2019-11-24 22:17:41 +0100

    Pour faire la fusion des branche

--- src/be/cuvelier/Main.java
+++ src/be/cuvelier/Main.java
@@ -7,9 +7,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Tournoi t = new Tournoi();
+
 		
+		Tournoi t = new Tournoi(Tournoi.SIMPLE_FEMME);
+		t.inscrireJoueur();
 		
+			t.jouer();
 
 	}
 
